80341ea46dde484b80ec6b94bf4e4c6aa326beb2
wednesday13th1 <kirin1013qilin@gmail.com>
2025-05-14 19:57:31 +0900

    Update koishikawamap.swift

--- Hiroo App/koishikawamap.swift
+++ Hiroo App/koishikawamap.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  koishikawamap.swift
 //  Hiroo App
@@ -9,13 +11,79 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITextViewDelegate {
   @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var urltextView: UITextView!
+  @IBOutlet weak var mitalinetextView: UITextView!
+    @IBOutlet weak var titletextView: UITextView!
+    @IBOutlet weak var bustextView: UITextView!
+  
+    
   var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self
+        
+        titletextView.layer.borderColor = UIColor.gray.cgColor
+        titletextView.layer.borderWidth = 1
+        titletextView.layer.cornerRadius = 8.0
+        titletextView.font = UIFont.systemFont(ofSize: 15)
+        titletextView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titletextView)
+        
+        urltextView.layer.borderColor = UIColor.gray.cgColor
+        urltextView.layer.borderWidth = 1
+        urltextView.layer.cornerRadius = 8.0
+        urltextView.font = UIFont.systemFont(ofSize: 15)
+        urltextView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(urltextView)
 
+        mitalinetextView.layer.borderColor = UIColor.gray.cgColor
+        mitalinetextView.layer.borderWidth = 1
+        mitalinetextView.layer.cornerRadius = 8.0
+        mitalinetextView.font = UIFont.systemFont(ofSize: 15)
+        mitalinetextView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mitalinetextView)
+        
+        bustextView.layer.borderColor = UIColor.gray.cgColor
+        titletextView.layer.borderWidth = 1
+        bustextView.layer.cornerRadius = 8.0
+        bustextView.font = UIFont.systemFont(ofSize: 15)
+        bustextView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bustextView)
+        
+        NSLayoutConstraint.activate([
+            
+            titletextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titletextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titletextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titletextView.heightAnchor.constraint(equalToConstant: 21),
+            
+            mitalinetextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            mitalinetextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mitalinetextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mitalinetextView.heightAnchor.constraint(equalToConstant: 21),
+            
+            bustextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            bustextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bustextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bustextView.heightAnchor.constraint(equalToConstant: 21),
+            
+            urltextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            urltextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            urltextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            urltextView.heightAnchor.constraint(equalToConstant: 21),
+        ])
+        
+        let baseString = "これは設定アプリへのリンクを含む文章です。\n\nこちらのリンクはGoogle検索です"
+        let attributedString = NSMutableAttributedString(string: baseString)
+        mapView.delegate = self
+        
+        attributedString.addAttribute(.link,
+                                            value: "https://www.jorudan.co.jp/",
+                                            range: NSString(string: baseString).range(of: "乗車案内"))
+              
+        urltextView.attributedText = attributedString
+        
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -27,8 +95,8 @@ class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDeleg
         mapView.setRegion(region, animated: true)
 
         let pin = MKPointAnnotation()
-        pin.title = "タイトル"
-        pin.subtitle = "サブタイトル"
+        pin.title = "広尾学園小石川"
+        pin.subtitle = "hiroo gakuen koishikawa"
         pin.coordinate = hgk
         mapView.addAnnotation(pin)
     }
@@ -41,5 +109,7 @@ class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDeleg
         return true
     }
 }
+
+
 
 
