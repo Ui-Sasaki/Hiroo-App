a3e2a8100609ea4a7583b3cc771d84c017b65673
Kirin Inoue <kirin1013qilin@gmail.com>
2025-06-03 12:45:30 +0900

    commit 6/3

--- Hiroo App/koishikawamap.swift
+++ Hiroo App/koishikawamap.swift
@@ -1,5 +1,3 @@
-
-
 //
 //  koishikawamap.swift
 //  Hiroo App
@@ -11,84 +9,74 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITextViewDelegate {
+class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var urltextView: UILabel!
     @IBOutlet weak var mitalinetextView: UILabel!
     @IBOutlet weak var titletextView: UILabel!
     @IBOutlet weak var bustextView: UILabel!
   
+    var locationManager: CLLocationManager!
     
-  var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        titletextView.layer.borderColor = UIColor.gray.cgColor
-//        titletextView.layer.borderWidth = 1
-//        titletextView.layer.cornerRadius = 8.0
-//        titletextView.font = UIFont.systemFont(ofSize: 15)
-//        titletextView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(titletextView)
-//        
-//        urltextView.layer.borderColor = UIColor.gray.cgColor
-//        urltextView.layer.borderWidth = 1
-//        urltextView.layer.cornerRadius = 8.0
-//        urltextView.font = UIFont.systemFont(ofSize: 15)
-//        urltextView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(urltextView)
-//
-//        mitalinetextView.layer.borderColor = UIColor.gray.cgColor
-//        mitalinetextView.layer.borderWidth = 1
-//        mitalinetextView.layer.cornerRadius = 8.0
-//        mitalinetextView.font = UIFont.systemFont(ofSize: 15)
-//        mitalinetextView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(mitalinetextView)
-//        
-//        bustextView.layer.borderColor = UIColor.gray.cgColor
-//        titletextView.layer.borderWidth = 1
-//        bustextView.layer.cornerRadius = 8.0
-//        bustextView.font = UIFont.systemFont(ofSize: 15)
-//        bustextView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(bustextView)
-        
+        // Set up constraints (commented out sections can be uncommented if needed)
         NSLayoutConstraint.activate([
+            // Uncomment these if you want to programmatically set constraints
+            /*
+            titletextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titletextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titletextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titletextView.heightAnchor.constraint(equalToConstant: 21),
             
-//            titletextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            titletextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            titletextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            titletextView.heightAnchor.constraint(equalToConstant: 21),
-//            
-//            mitalinetextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            mitalinetextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            mitalinetextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            mitalinetextView.heightAnchor.constraint(equalToConstant: 21),
-//            
-//            bustextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            bustextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            bustextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            bustextView.heightAnchor.constraint(equalToConstant: 21),
-//            
-//            urltextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-//            urltextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            urltextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            urltextView.heightAnchor.constraint(equalToConstant: 21),
+            mitalinetextView.topAnchor.constraint(equalTo: titletextView.bottomAnchor, constant: 10),
+            mitalinetextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mitalinetextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mitalinetextView.heightAnchor.constraint(equalToConstant: 21),
+            
+            bustextView.topAnchor.constraint(equalTo: mitalinetextView.bottomAnchor, constant: 10),
+            bustextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bustextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bustextView.heightAnchor.constraint(equalToConstant: 21),
+            
+            urltextView.topAnchor.constraint(equalTo: bustextView.bottomAnchor, constant: 10),
+            urltextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            urltextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            urltextView.heightAnchor.constraint(equalToConstant: 21),
+            */
         ])
         
-        let baseString = "これは設定アプリへのリンクを含む文章です。\n\nこちらのリンクはGoogle検索です"
+        // Set up the URL text with attributed string
+        let baseString = "これは設定アプリへのリンクを含む文章です。\n\n乗車案内はこちらのリンクです"
         let attributedString = NSMutableAttributedString(string: baseString)
+        
+        // Add link attribute to specific text
+        if let range = baseString.range(of: "乗車案内") {
+            let nsRange = NSRange(range, in: baseString)
+            attributedString.addAttribute(.link,
+                                        value: "https://www.jorudan.co.jp/",
+                                        range: nsRange)
+        }
+        
+        // For UILabel, you need to enable user interaction to make links tappable
+        urltextView.attributedText = attributedString
+        urltextView.isUserInteractionEnabled = true
+        
+        // Add tap gesture to handle link taps
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleLinkTap(_:)))
+        urltextView.addGestureRecognizer(tapGesture)
+        
+        // Set up map
         mapView.delegate = self
         
-        attributedString.addAttribute(.link,
-                                            value: "https://www.jorudan.co.jp/",
-                                            range: NSString(string: baseString).range(of: "乗車案内"))
-              
-        urltextView.attributedText = attributedString
-        
+        // Set up location manager
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
 
+        // Set up map region and annotation
         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let hgk = CLLocationCoordinate2DMake(35.728851235181615, 139.7468261848643)
         let region = MKCoordinateRegion(center: hgk, span: span)
@@ -100,6 +88,14 @@ class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDeleg
         pin.coordinate = hgk
         mapView.addAnnotation(pin)
     }
+    
+    @objc func handleLinkTap(_ gesture: UITapGestureRecognizer) {
+        // Handle the link tap - open URL
+        if let url = URL(string: "https://www.jorudan.co.jp/") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
                 let url = userActivity.webpageURL else {
@@ -109,7 +105,3 @@ class koishikawamap: UIViewController, CLLocationManagerDelegate, MKMapViewDeleg
         return true
     }
 }
-
-
-
-
