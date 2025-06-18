ce18037bc62c6e7fff69dd991703da6f3d9a4d26
Ui-Sasaki <uiuisasaki@gmail.com>
2025-06-18 20:17:29 +0900

    a few things

---
+++ Hiroo App/CongestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CongestionViewController.swift
+//  Hiroo App
+//
+//  Created by ard on 2025/06/18.
+//
+
+import UIKit
+
+class CongestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hiroo App/TimeTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimeTableViewController.swift
+//  Hiroo App
+//
+//  Created by ard on 2025/06/18.
+//
+
+import UIKit
+
+class TimeTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
