d968c58bad5365564c8e8f2ce5213f26e070ca6b
Kirin Inoue <kirin1013qilin@gmail.com>
2025-07-23 20:38:14 +0900

    7/23 commit

---
+++ Hiroo App/CongestionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CongestionTableViewCell.swift
+//  Hiroo App
+//
+//  Created by 井上　希稟 on 2025/07/23.
+//
+
+import UIKit
+
+class CongestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
